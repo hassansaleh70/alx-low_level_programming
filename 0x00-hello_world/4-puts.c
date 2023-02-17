9b5b0b632484498d1ef8646a69200c65d48050d8
hassansaleh70 <salehaboya3423@gmail.com>
2023-02-16 18:55:26 -0800

    mnjhuy777uyu

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
-* main - A program to print in puts
-* Return: 0 (Success) 
-*/
+ * main - programm to print something by puts
+ * Return: 0 (Success)
+ */
 int main(void)
 {
-puts("Programming is like building a multilingual puzzle");
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
-* main - A program to print in puts
-* Return: 0 (Success) 
-*/
+ * main - program to output by printf
+ * Return: 0 (Success)
+ */
 int main(void)
 {
-puts("with proper grammar, but the outcome is a piece of art,");
-return (0);
+        printf("with proper grammar, but the outcome is a piece of art,\n");
+        return (0);
 }
