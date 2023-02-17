c7220dfea3b78d9374538d8495364500a20a79ea
hassansaleh70 <salehaboya3423@gmail.com>
2023-02-16 18:36:34 -0800

    9i9i98uuu8

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+* main - A program to print in puts
+* Return: 0 (Success) 
+*/
+int main(void)
+{
+puts("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
